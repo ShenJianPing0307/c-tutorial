3e7b2800a21336583f222a3284df492953b2c58f
ShenJianPing0307 <610502705@qq.com>
2023-04-06 11:00:59 +0800

    初识C语言

---
+++ c_code/1.1.3 qthb_demo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	// 定义整型变量
+	int a, b, c;
+	__asm
+	{
+		
+		mov a, 3 // 3的值放入a对应的内存位置
+		mov b, 4 // 4的值放入b对应的内存位置
+		mov eax, a //把a的值放在eax寄存器
+		add eax, b //eax和b相加, 结果放在eax
+		mov c, eax //eax的值放在c中
+	}
+	printf("%d\n", c);
+	return 0;
+
+}
